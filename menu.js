655fbf6c3deb1233083004b027d23da75b20e676
jesusalbertobaute <jesusalbertobaute@gmail.com>
2023-06-07 00:18:37 -0400

    Added Home Page and Rock,Paper,Scissor game

---
+++ menu.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', (eventLoad) => {
+    const menuItemGame1= document.getElementById("game1");
+
+    menuItemGame1.addEventListener('click',(e)=>{
+        window.location.href="modules/rock-paper-scissor/rock-paper-scissor.html";
+    });
+
+});
---
+++ modules/rock-paper-scissor/js/rock-paper-scissor.js
@@ -0,0 +1,78 @@
+window.addEventListener('DOMContentLoaded', (eventLoad) => {
+    const computerChoiceDisplay=document.getElementById('computer-choice');
+    const userChoiceDisplay=document.getElementById('user-choice');
+    const winnerDisplay=document.getElementById('winner');
+    const choicesButton= document.getElementsByClassName("choice-button");
+    const gamesUserWinResultDisplay= document.getElementById("games-win-user");
+    const gamesComputerWinResultDisplay= document.getElementById("games-win-computer");
+    const gamesTiedResultDisplay= document.getElementById("games-tied");
+    const backHomeButton= document.getElementById("rps-back-menu-button");
+
+
+    backHomeButton.addEventListener('click',(e)=>{
+        window.location.href="../../index.html";
+    });
+
+    const options= ["rock","paper","scissor"];
+    let gamesUserWin=0;
+    let gamesComputerWin=0; 
+    let gamesTied=0;
+
+    const updateResults= (result) => {
+         if (result==='computer'){
+            ++gamesComputerWin;
+         }else if (result==='user'){
+             ++gamesUserWin;
+         }else{
+            ++gamesTied;
+         }
+
+        gamesComputerWinResultDisplay.innerHTML= gamesComputerWin;
+        gamesUserWinResultDisplay.innerHTML= gamesUserWin;
+        gamesTiedResultDisplay.innerHTML= gamesTied;
+
+    };
+
+    const generateComputerChoice= (userChoice) => {
+       let computerChoice= options[(Math.random() * 2).toFixed()];
+       computerChoiceDisplay.innerHTML= computerChoice.toUpperCase();
+
+       if (userChoice===computerChoice){
+            winnerDisplay.innerHTML= "TIED";
+            updateResults("tied");
+            return;
+       }
+
+       if ((userChoice===options[0] && computerChoice===options[2]) ||
+           (userChoice===options[1] && computerChoice===options[0]) ||
+           (userChoice===options[2] && computerChoice===options[1])){
+          winnerDisplay.innerHTML= "YOU WIN!!!";
+          updateResults("user");
+          return;
+       }
+
+        updateResults("computer");
+        winnerDisplay.innerHTML= "COMPUTER WIN!!!";
+
+    };
+
+    [...choicesButton].forEach(choiceButton => {
+
+         choiceButton.addEventListener("click",(eventButton)=>{
+            let userChoice="";
+            if (eventButton.target.id.search('rock')>-1){
+                userChoice=options[0];
+            }else if(eventButton.target.id.search('paper')>-1){
+                userChoice=options[1];
+            }else if(eventButton.target.id.search('scissor')>-1){
+                userChoice=options[2];
+            }
+            userChoiceDisplay.innerHTML= userChoice.toUpperCase();
+            generateComputerChoice(userChoice);
+         });
+
+    });
+});
+
+
+
